213e25739cdd8ebc9e777bab40009dbf7ebf82c6
Joshua Kent <jkent6363@gmail.com>
2020-11-09 10:17:01 -0500

    main project set up

---
+++ FaceWatch/FaceWatch/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  FaceWatch
+//
+//  Created by Joshua Kent on 11/9/20.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var faceLabel: UILabel!
+    
+    var analysis = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        //sceneView.showsStatistics = true
+        
+        
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Face tracking is not supported on this device")
+        }
+        
+        //round faceLabel adges
+        faceLabel.layer.cornerRadius = 5;
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARFaceTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let faceMesh = ARSCNFaceGeometry(device: sceneView.device!)
+        let node = SCNNode(geometry: faceMesh)
+        node.geometry?.firstMaterial?.fillMode = .lines
+     
+        return node
+    }
+    
+    //This updates the AR
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
+            //updates face mesh
+            faceGeometry.update(from: faceAnchor.geometry)
+            //calls expression function to determine facial expressions
+            expression(anchor: faceAnchor)
+        
+            DispatchQueue.main.async {
+                self.faceLabel.text = self.analysis
+            }
+        }
+    }
+
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    func expression(anchor: ARFaceAnchor) {
+        let smileLeft = anchor.blendShapes[.mouthSmileLeft]
+        let smileRight = anchor.blendShapes[.mouthSmileRight]
+        let cheekPuff = anchor.blendShapes[.cheekPuff]
+        let tongue = anchor.blendShapes[.tongueOut]
+        self.analysis = ""
+     
+        if ((smileLeft?.decimalValue ?? 0.0) + (smileRight?.decimalValue ?? 0.0)) > 0.9 {
+            self.analysis += "You are smiling. "
+        }
+     
+        if cheekPuff?.decimalValue ?? 0.0 > 0.1 {
+            self.analysis += "Your cheeks are puffed. "
+        }
+     
+        if tongue?.decimalValue ?? 0.0 > 0.1 {
+            self.analysis += "Your tongue is out. "
+        }
+    }
+}
