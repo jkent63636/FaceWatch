47cde3daa079b05ebefd06a718b514fa6ac8c4a7
Joshua Kent <jkent6363@gmail.com>
2020-11-10 12:39:45 -0500

    update face mesh and description

--- FaceWatch/FaceWatch/ViewController.swift
+++ FaceWatch/FaceWatch/ViewController.swift
@@ -12,9 +12,13 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
-    @IBOutlet weak var faceLabel: UILabel!
+    @IBOutlet weak var facialMovementLabel: UILabel!
     
-    var analysis = ""
+    //string of movements
+    var facialMovementDescription = ""
+    
+    //face geometry. Global variable made to turn mesh on/off
+    var faceGeo: ARSCNFaceGeometry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +29,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         //sceneView.showsStatistics = true
         
+        //curve label corners
+        facialMovementLabel.layer.cornerRadius = 10
         
         guard ARFaceTrackingConfiguration.isSupported else {
             fatalError("Face tracking is not supported on this device")
         }
-        
-        //round faceLabel adges
-        faceLabel.layer.cornerRadius = 5;
         
     }
     
@@ -59,20 +62,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let faceMesh = ARSCNFaceGeometry(device: sceneView.device!)
         let node = SCNNode(geometry: faceMesh)
         node.geometry?.firstMaterial?.fillMode = .lines
-     
+
         return node
     }
     
     //This updates the AR
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         if let faceAnchor = anchor as? ARFaceAnchor, let faceGeometry = node.geometry as? ARSCNFaceGeometry {
+            
+            faceGeo = faceGeometry
+            
             //updates face mesh
             faceGeometry.update(from: faceAnchor.geometry)
+                
             //calls expression function to determine facial expressions
             expression(anchor: faceAnchor)
         
             DispatchQueue.main.async {
-                self.faceLabel.text = self.analysis
+                self.facialMovementLabel.text = self.facialMovementDescription
             }
         }
     }
@@ -98,18 +105,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let smileRight = anchor.blendShapes[.mouthSmileRight]
         let cheekPuff = anchor.blendShapes[.cheekPuff]
         let tongue = anchor.blendShapes[.tongueOut]
-        self.analysis = ""
+        let blinkLeft = anchor.blendShapes[.eyeBlinkLeft]
+        let blinkRight = anchor.blendShapes[.eyeBlinkRight]
+        
+        facialMovementDescription = ""
      
         if ((smileLeft?.decimalValue ?? 0.0) + (smileRight?.decimalValue ?? 0.0)) > 0.9 {
-            self.analysis += "You are smiling. "
+            if !facialMovementDescription.isEmpty {
+                facialMovementDescription += "\n"
+            }
+            facialMovementDescription += "Smiling"
         }
      
         if cheekPuff?.decimalValue ?? 0.0 > 0.1 {
-            self.analysis += "Your cheeks are puffed. "
+            if !facialMovementDescription.isEmpty {
+                facialMovementDescription += "\n"
+            }
+            facialMovementDescription += "Cheeks Puffed"
         }
      
         if tongue?.decimalValue ?? 0.0 > 0.1 {
-            self.analysis += "Your tongue is out. "
+            if !facialMovementDescription.isEmpty {
+                facialMovementDescription += "\n"
+            }
+            facialMovementDescription += "Tongue Out"
+        }
+        
+        //actually right eye
+        if blinkLeft?.decimalValue ?? 0.0 > 0.6 {
+            if !facialMovementDescription.isEmpty {
+                facialMovementDescription += "\n"
+            }
+            facialMovementDescription += "Your Right Eye Blink"
+        }
+
+        //actually left eye
+        if blinkRight?.decimalValue ?? 0.0 > 0.6 {
+            if !facialMovementDescription.isEmpty {
+                facialMovementDescription += "\n"
+            }
+            facialMovementDescription += "Your Left Eye Blink"
         }
     }
+    
+    //turns face mesh on and off based on switch
+    @IBAction func faceMeshSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            faceGeo?.firstMaterial?.transparency = 100
+        } else {
+            faceGeo?.firstMaterial?.transparency = 0
+        }
+    }
+    
 }
